2a1b3899c25e6f1a971b8831d43bf8a6be42f558
Aiyzihk <45091852+Isaac-onah@users.noreply.github.com>
2022-04-24 12:16:20 +0100

    update

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -98,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
                          ],
                        ),
                        SizedBox(height:12),
-                       Text("Ready to Lea", style: AppStyle.b32w,),
+                       Text("Ready to", style: AppStyle.b32w,),
                        SizedBox(height:4),
                        Text("Choose your league", style: AppStyle.r12w,),
                      ],
