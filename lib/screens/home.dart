a965ce0fd5608f7605c6f2f76ec71337fcf4e212
Aiyzihk <45091852+Isaac-onah@users.noreply.github.com>
2022-04-25 12:16:20 +0100

    update

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -98,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
                          ],
                        ),
                        SizedBox(height:12),
-                       Text("get live scorea", style: AppStyle.b32w,),
+                       Text("get live scora", style: AppStyle.b32w,),
                        SizedBox(height:4),
                        Text("Cor league", style: AppStyle.r12w,),
                      ],
