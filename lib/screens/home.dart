869250cc64210183a8dac2d4578a0f66dbd0aa66
Aiyzihk <45091852+Isaac-onah@users.noreply.github.com>
2022-04-28 12:16:20 +0100

    update

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -94,7 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
                      children: [
                        Row(
                          children: [
-                           Text("Catery", style: AppStyle.m12w,)
+                           Text("Catey", style: AppStyle.m12w,)
                          ],
                        ),
                        SizedBox(height:12),
