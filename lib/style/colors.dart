4bb3910d05c30811e490b77dec035107b7318c1d
Aiyzihk <45091852+Isaac-onah@users.noreply.github.com>
2022-07-09 12:16:20 +0100

    update

--- lib/style/colors.dart
+++ lib/style/colors.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 abstract class AppColors{
   static const Color red = Color(0xFFFF6474);
   static const Color orange = Color(0xFFFFAA5B);
-  static const Color purple = Color(0xFF9AA0FC);
+  static const Color purple = Color(0xFF0C3B2E);
   static const Color pink = Color(0xFFFBAEAE);
   static const Color green = Color(0xFF71CFB9);
 }
