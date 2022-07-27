1efdee050c49f304711b98fc7dd8b102de87098a
Aiyzihk <45091852+Isaac-onah@users.noreply.github.com>
2022-07-27 10:33:33 +0100

    update

---
+++ lib/screens/widgets.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class matchLabel extends StatelessWidget {
+  final String home;
+  final String homelogo;
+  final String homescore;
+  final String awayscore;
+  final String awaylogo;
+  final String away;
+  const matchLabel({ required this.home, required this.homelogo, required this.homescore, required this.awayscore, required this.awaylogo, required this.away
+  });
+  @override
+  Widget build(BuildContext context) {
+    return  Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: 8.0, vertical: 15),
+          child: Center(
+            child: Row(
+              children: [
+                Expanded(
+                    child: Text(
+                      home,
+                      textAlign: TextAlign.end,
+                    )),
+                Padding(
+                  padding:
+                  EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Image.asset(
+                    homelogo,
+                    width: 30,
+                  ),
+                ),
+                Container(
+                  color: Colors.pink,
+                  width: 60,
+                  height: 30,
+                  child: Padding(
+                    padding: EdgeInsets.all(5.0),
+                    child: Row(
+                      mainAxisAlignment:
+                      MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(homescore),
+                        VerticalDivider(),
+                        Text(awayscore),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding:
+                  EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Image.asset(
+                    awaylogo,
+                    width: 30,
+                  ),
+                ),
+                Expanded(child: Text(away)),
+              ],
+            ),
+          ),
+        ),
+        Divider(
+          thickness: 2,
+        ),
+      ],
+    );
+  }
+}
